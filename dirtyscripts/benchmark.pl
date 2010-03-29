b681b9f1988f33aca762efd8bdafaf98149b1a9d
Boris Däppen <boris_daeppen@bluewin.ch>
2010-03-29 15:44:36 +0100

    basic idea works now. todo list is in the file

--- dirtyscripts/benchmark.pl
+++ dirtyscripts/benchmark.pl
@@ -18,11 +18,16 @@
 # along with orgcreator.  If not, see <http://www.gnu.org/licenses/>.
 
 
-# hack to enable proxy for LWP::Simple
-BEGIN { $ENV{HTTP_proxy}="http://192.168.0.1:3128" }
+# TODO
+#  * regex replace of url with IP so that also images can be received
+#  * better  and more test-url's
+#  * nice shell output
+#  * error handling (maybe just abort. ore just ignore and take the time?)
+
 
 use strict;
 use warnings;
+use Time::HiRes qw(time);
 use LWP::Simple;
 use Net::DNS::Resolver; # uses libnet-dns-perl package in ubuntu
 use GD::Graph::lines;   # uses libgd-graph-perl
@@ -45,17 +50,21 @@ elsif ($ARGV[0] =~ /[^0-9]/) {
 
 # initialise things I need
 my $rounds = $ARGV[0];
-my @pages = ('http://www.google.com/', 'http://times.com/', 'http://www.bbc.co.uk/', 'http://sourceforge.net/', 'http://hackles.org/');
-my $totaltime = 0;
-my %result = ();
-my $errors = 0;
 my $highest = 0;
+my @time_proxy = ();
+my @time_noproxy = ();
+
+my @pages = (   'http://www.google.com/',
+                'http://www.bbc.co.uk/',
+                #'http://www.gnu.org/graphics/heckert_gnu.small.png',
+                'http://hackles.org/');
 
 my $dns_resolver = Net::DNS::Resolver->new(
       nameservers => [qw(192.168.0.1)],
       recurse     => 1,
       debug       => 0,
 );
+
 
 # loop through all pages, download them
 for (my $i=1;$i<=$rounds;$i++) {
@@ -65,88 +74,31 @@ for (my $i=1;$i<=$rounds;$i++) {
         if ($url =~ /\/\/(.*)\//) {
             $saveurl = $1;
         }
+
         my $start = time();
-        print "Round $i:\ttry to receive $saveurl...";
-
-        my $query = $dns_resolver->search($saveurl);
-        my $ip = 0;
-        if ($query) {
-            foreach my $rr ($query->answer) {
-                next unless $rr->type eq "A";
-                $ip = $rr->address;
-                last; # break the loop
-            }
-        } else {
-            warn "\nquery failed: ", $dns_resolver->errorstring, "\n";
-        }
-
-
-        print "\ton $ip";
-        my $status = getstore ("http://$ip", "benchmark/$saveurl");
-
+        print "PROXY\tRound $i:\ttry to receive $saveurl...";
+        ask_nayaboh($saveurl);
         my $end = time();
-        my $duration = $end - $start;
-        if (is_error($status)) {
-            $duration = 0;
-            $errors++;
-            $result{"Round $i"}{"$saveurl"} = 'ERROR';
-            print "\tDONE\tERROR:\t$status\n";
-        }
-        else {
-            $result{"Round $i"}{"$saveurl"} = $duration;
-            if ($duration > $highest) { $highest = $duration; }
-            print "\tDONE\t$duration sec\n";
-        }
+        my $duration = sprintf("%.1f", ($end - $start));
+        push (@time_proxy, $duration);
+        if ($duration > $highest) { $highest = $duration; }
+        print "\tDONE\t$duration sec\n";
 
-        $totaltime += $duration;
+
+        $start = time();
+        print "DIRECT\tRound $i:\ttry to receive $saveurl...";
+        ask_internet($saveurl);
+        $end = time();
+        $duration = sprintf("%.1f", ($end - $start));
+        push (@time_noproxy, $duration);
+        if ($duration > $highest) { $highest = $duration; }
+        print "\tDONE\t$duration sec\n";
+
     }
 }
 
-# generate some intelligent output
-print "\n\nRESULTS:\n\n";
-foreach my $url (keys %{$result{"Round 1"}}) {
-    print $url;
-    print "\t";
-}
-print "\n";
-foreach my $round (keys %result) {
-    foreach my $url (keys %{$result{$round}}) {
-        print "$result{$round}{$url}\t";
-    }
-        print "\n";
-}
-print "\n\nCALCULATIONS:\n\n";
-my $req_count = (scalar(@pages) * $rounds) - $errors;
-unless ($req_count) { die "NO REQUESTS MADE. $errors ERRORS.\n\n"; }
-print "receiving of " . $req_count . " pages took $totaltime seconds.\n";
-print "AVARAGE: " . int( ($totaltime / $req_count) + 0.5) . " seconds per page.\n";
-print "\nThere where $errors ERRORS.\n\n";
 
-#use Data::Dumper;
-#print "\n\n\nDEBUG:\n";
-#print Dumper(\%result);
-
-my %grapharrays = ();
-foreach my $round (keys %result) {
-    foreach my $url (keys %{$result{$round}}) {
-        if ($result{$round}{$url} =~ /ERROR/) {
-            push (@{$grapharrays{$url}}, undef);
-        }
-        else {
-            push (@{$grapharrays{$url}}, $result{$round}{$url});
-        }
-    }
-}
-
-my @data = ( [1 .. $rounds] );
-foreach my $bla (keys %grapharrays) {
-    push (@data, $grapharrays{$bla});
-}
-use Data::Dumper;
-print "\n\n\nDEBUG:\n";
-print Dumper(\@data);
-
-my $graph = GD::Graph::lines->new(800, 600);
+my $graph = GD::Graph::lines->new(800, 400);
 
 $graph->set( 
     x_label           => "$rounds Rounds",
@@ -157,10 +109,62 @@ $graph->set(
     y_label_skip      => 1 
 ) or die $graph->error;
 
-my $gd = $graph->plot(\@data) or die $graph->error;
+my @sorted_time_proxy = sort {$a <=> $b } @time_proxy;
+my @sorted_time_noproxy = sort {$a <=> $b } @time_noproxy;
+
+my @data = (    [1 .. ($rounds * scalar @pages)],
+                \@sorted_time_noproxy,
+                \@sorted_time_proxy );
+
+#use Data::Dumper;
+#print Dumper(\@data);
+
+$graph->set_legend('Direct', 'NaYaBoh');
+my $gd = $graph->plot( \@data ) or die $graph->error;
 
 open(IMG, '>file.png') or die $!;
 binmode IMG;
 print IMG $gd->png;
 close IMG;
 
+
+
+
+
+sub ask_nayaboh {
+
+    my $url = shift;
+
+    my $any_error = 1;
+
+    my $query = $dns_resolver->search($url);
+    my $ip = 0;
+    if ($query) {
+        foreach my $rr ($query->answer) {
+            next unless $rr->type eq "A";
+            $ip = $rr->address;
+            last; # break the loop
+        }
+    } else {
+        warn "\nquery failed: ", $dns_resolver->errorstring, "\n";
+        $any_error = 0;
+    }
+
+    my $status = `perl -e "BEGIN { \\\$ENV{HTTP_proxy}='http://192.168.0.1:3128' }; use LWP::Simple; print getstore ('http://$ip', 'bla');"`;
+
+    if (is_error($status)) { $any_error = 0; }
+
+    return $any_error;
+}
+
+sub ask_internet {
+
+    my $url = shift;
+    my $any_error = 1;
+
+    my $status = getstore ("http://$url", $url);
+
+    if (is_error($status)) { $any_error = 0; }
+
+    return $any_error;
+}
